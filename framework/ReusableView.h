38fef69b87e75614aa0993c51102bb9f98127ce6
eMdOS <emilio.ojeda.mendoza@gmail.com>
2018-11-01 23:08:57 -0600

    fix: rename the public header file

--- framework/ReusableView.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-//! Project version number for ReusableView.
-FOUNDATION_EXPORT double ReusableViewVersionNumber;
-
-//! Project version string for ReusableView.
-FOUNDATION_EXPORT const unsigned char ReusableViewVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <ReusableView/PublicHeader.h>
---
+++ framework/ReusableViewKit.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReusableViewKit.
+FOUNDATION_EXPORT double ReusableViewKitVersionNumber;
+
+//! Project version string for ReusableViewKit.
+FOUNDATION_EXPORT const unsigned char ReusableViewKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReusableViewKit/PublicHeader.h>
